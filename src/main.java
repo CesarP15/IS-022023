642b8bd4e301d875880d4809a7f74e44c0cfc602
Cesarp15 <cesar150595@gmail.com>
2024-11-08 21:04:53 -0400

    Estructura inical

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void HelloWorld(String[] args){
+
+        System.out.println("Hello World! por segunda vez");
+        System.out.println("se logro?");
+    }
+}
